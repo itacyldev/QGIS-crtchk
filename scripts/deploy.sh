cf6920c814993b52f448b1bcfc1904fad599b53a
gusriobr <gusriobr@gmail.com>
2023-01-27 13:40:04 +0100

    avoid copying venv

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,4 @@
 cp -R /media/gus/workspace/wpy/QGIS-crtchk /home/gus/.local/share/QGIS/QGIS3/profiles/default/python/plugins/
 rm -rf /home/gus/.local/share/QGIS/QGIS3/profiles/default/python/plugins/cartodruid_sync
 mv /home/gus/.local/share/QGIS/QGIS3/profiles/default/python/plugins/QGIS-crtchk /home/gus/.local/share/QGIS/QGIS3/profiles/default/python/plugins/cartodruid_sync
+rm -rf /home/gus/.local/share/QGIS/QGIS3/profiles/default/python/plugins/cartodruid_sync/venv
