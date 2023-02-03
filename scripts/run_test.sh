cfca7647c46aea9c6c8ba79bb638ba3fafb40a9f
gusriobr <gusriobr@gmail.com>
2023-02-03 08:00:35 +0100

    configures xml unit reports

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,4 +1,4 @@
 # tun this script from project root
 #python3 -m unittest discover . -s test -v
 pip install xmlrunner
-python3 -m xmlrunner discover . -s test -v -o build/test-reports
+python3 -m xmlrunner discover . -s test -v -o build/test-results
