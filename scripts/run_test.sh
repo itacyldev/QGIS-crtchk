52395a3ee9460c0bb8e12f563b1fefb0d5df740e
gusriobr <gusriobr@gmail.com>
2023-02-03 08:05:15 +0100

    fixes error in workflow.yml

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,4 +1,4 @@
 # tun this script from project root
 #python3 -m unittest discover . -s test -v
-pip install xmlrunner
+pip install unittest-xml-reporting
 python3 -m xmlrunner discover . -s test -v -o build/test-results
