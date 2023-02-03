f390a5b17f3e9dd89dea32420ba37a615a45e390
gusriobr <gusriobr@gmail.com>
2023-02-03 08:42:08 +0100

    run coverage report

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,4 +1,6 @@
 # tun this script from project root
 #python3 -m unittest discover . -s test -v
 pip install unittest-xml-reporting
-python3 -m xmlrunner discover . -s test -v -o build/test-results
+pip install coverage
+
+coverage run --data-file=build/coverage-reports/.coverage -m xmlrunner discover . -s test -v -o build/test-results
